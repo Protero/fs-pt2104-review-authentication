cb50a4eadd4cafc91f8672da98d023e80247f6ee
Protero <protero@outlook.es>
2021-07-29 20:01:36 +0200

    Created: sql script

---
+++ exercise-auth-sql/sql/creation.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users;
+DROP EXTENSION IF EXISTS "uuid-ossp";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    hash TEXT NOT NULL,
+    birthdate DATE NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT FALSE,
+    confirmation_token TEXT DEFAULT NULL,
+    profile_pic TEXT DEFAULT NULL,
+    bio TEXT DEFAULT '',
+    deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
+    update_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC')
+);
