92cb9f03e3eb3cc8a14b97d0bef8b106bf72706c
Protero <protero@outlook.es>
2021-08-29 19:50:10 +0200

    Added: createConfirmToken

--- exercise-auth-sql/helpers/hash.js
+++ exercise-auth-sql/helpers/hash.js
@@ -1,11 +1,15 @@
 const bcrypt =  require('bcrypt');
+const crypto = require('crypto');
 
 const encryt = async hash => await bcrypt.hash(hash,await bcrypt.genSalt(Number(process.env.SALT)));
     
 const compare = hashTxt => async hash => await bcrypt.compare(hashTxt,hash);
 
+const createConfirmToken = () => crypto.randomBytes(32).toString('hex');
+
 
 module.exports={
     encryt,
     compare,
+    createConfirmToken,
 }
--- exercise-auth-sql/helpers/index.js
+++ exercise-auth-sql/helpers/index.js
@@ -1,16 +1,21 @@
 const {createCookie,clearCookie } = require('./cookies');
-const {encryt,compare} = require('./hash');
+const {encryt,compare,createConfirmToken} = require('./hash');
 const {toJWT,fromJWT} = require('./jwt');
+const {sendMail} = require('./mailer');
 
 const serialaze = (res,email,username) => createCookie(res,toJWT(email,username));
 
 
-const deserialaze = () => {};
+const deserialaze = req => {
+    const {token} = req.cookies;
+    return fromJWT(token);
+};
 
 module.exports = {
     cookies : {createCookie,clearCookie},
-    hashs : {encryt,compare},
+    hashs : {encryt,compare,createConfirmToken},
     jwt : {toJWT,fromJWT},
     serialaze,
     deserialaze,
+    sendMail,
 };
