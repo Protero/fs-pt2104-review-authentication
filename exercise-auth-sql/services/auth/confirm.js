3c681615e881813aa18b12e4e736533d757c7dac
Protero <protero@outlook.es>
2021-08-29 22:02:27 +0200

    Modified: confirm with resulMAilFinal

--- exercise-auth-sql/services/auth/confirm.js
+++ exercise-auth-sql/services/auth/confirm.js
@@ -1,4 +1,5 @@
 const {confirmUser} = require('../../queries/auth');
+const {mail} = require('../../helpers');
 
 module.exports = db => async (req,res,next) => {
     const {confirmationToken} = req.params;
@@ -8,6 +9,12 @@ module.exports = db => async (req,res,next) => {
     const result = await confirmUser(db,confirmationToken);
 
     if (!result) next({error: new Error('Token inválido')});
+    
+    const to =  result.rows[0].email;
+    const username = result.rows[0].username;
+    const resultMailFinal = await mail.sendMailFinal(to,username);
+    
+    if (resultMailFinal === false) return next({error: new Error('Algo ha ido mal en el envío de correo final')});
 
     res.status(200).json({
         succes: true,
