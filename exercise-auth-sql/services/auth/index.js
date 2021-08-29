465c97c45323407bc3ef9007cdaba2b6a778747b
Protero <protero@outlook.es>
2021-08-29 20:39:22 +0200

    Added: reference confirmuser in auth/index

--- exercise-auth-sql/services/auth/index.js
+++ exercise-auth-sql/services/auth/index.js
@@ -6,9 +6,11 @@ module.exports = db =>{
     
     const signin = require('./signin');
     const signup = require('./signup');
+    const confirm = require('./confirm');
 
     router.post('/signin',signin(db));
     router.post('/signup', signup(db));
+    router.get('/confirmation/:confirmationToken', confirm(db));
 
     return router;
 }
