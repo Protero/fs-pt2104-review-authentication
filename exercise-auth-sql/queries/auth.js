7d89eb32bdf2c08241f7576464ae4b7da389b1aa
Protero <protero@outlook.es>
2021-08-30 17:50:19 +0200

    Modified: addede query userMail and username

--- exercise-auth-sql/queries/auth.js
+++ exercise-auth-sql/queries/auth.js
@@ -39,6 +39,65 @@ const getUser = async (db, email, fn) => {
 
 };
 
+const getUserMail = async (db, email) => {
+    try {
+        const result = await db.maybeOne(sql
+            `
+                SELECT email, username
+                FROM users
+                WHERE email LIKE ${email} AND
+                active=true 
+            `);
+
+        if (!result) throw new Error('mail incorrecto. Más adelante modificaré el texto');    
+
+        return result;
+
+    } catch (error) {
+        console.log('> error at "getUserEmail" query: ', error.message);
+        return false;
+    }
+
+};
+
+const getUserUsername = async (db, username) => {
+    try {
+        const result = await db.maybeOne(sql
+            `
+                SELECT email, username
+                FROM users
+                WHERE username LIKE ${username} AND
+                active=true 
+            `);
+
+        if (!result) throw new Error('username incorrecto. Más adelante modificaré el texto');    
+
+        return result;
+
+    } catch (error) {
+        console.log('> error at "getUserEmail" query: ', error.message);
+        return false;
+    }
+
+};
+const chageTokenPassword = async (db, email, confirmationToken) => {
+    try {
+        return await db.query(sql
+            `UPDATE users
+            SET
+                email = ${email},
+                confirmation_token = ${confirmationToken}
+            WHERE
+                email like ${email}
+            RETURNING email, username;
+            `
+        );
+    } catch (error) {
+        console.info('error at "confirmUser" query: ', error.message);
+        return false;
+    }
+}
+
 const confirmUser = async (db, confirmationToken) => {
     try {
         return await db.query(sql
@@ -60,5 +119,7 @@ const confirmUser = async (db, confirmationToken) => {
 module.exports = {
     createUser,
     getUser,
+    getUserMail,
+    getUserUsername,
     confirmUser,
 };
