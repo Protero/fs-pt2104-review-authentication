979fb2a9fcd0f764c5e8f11c4fd9f1bd25d35cf4
Protero <protero@outlook.es>
2021-08-30 16:09:23 +0200

    Modified: Auth/index with signout rute

--- exercise-auth-sql/services/auth/index.js
+++ exercise-auth-sql/services/auth/index.js
@@ -1,4 +1,5 @@
 const { sign } = require('jsonwebtoken');
+const {authorizer} = require('../../middlewares/authorizer');
 
 const router =  require('express').Router();
 
@@ -7,10 +8,12 @@ module.exports = db =>{
     const signin = require('./signin');
     const signup = require('./signup');
     const confirm = require('./confirm');
+    const signout = require('./signout')
 
     router.post('/signin',signin(db));
     router.post('/signup', signup(db));
     router.get('/confirmation/:confirmationToken', confirm(db));
+    router.post('/signout', signout());
 
     return router;
 }
