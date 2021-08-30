019fce1729c608da1286b3428f4584c12be4e566
Protero <protero@outlook.es>
2021-08-30 17:51:45 +0200

    Modified: added new route to reqNewPassword

--- exercise-auth-sql/services/auth/index.js
+++ exercise-auth-sql/services/auth/index.js
@@ -8,12 +8,14 @@ module.exports = db =>{
     const signin = require('./signin');
     const signup = require('./signup');
     const confirm = require('./confirm');
-    const signout = require('./signout')
+    const signout = require('./signout');
+    const reqNewPassword = require('./reqNewPassword');
 
     router.post('/signin',signin(db));
     router.post('/signup', signup(db));
     router.get('/confirmation/:confirmationToken', confirm(db));
     router.post('/signout', signout());
+    router.post('/newPassword', reqNewPassword(db));
 
     return router;
 }
